ec340090f3c31edfd5f557b204609ca9deb2b647
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-19 00:15:32 +0530

    [SaiVenkat]-Prime Numbers between 1 to 1000

---
+++ Prime/Program.cs
@@ -0,0 +1,13 @@
+﻿using prime;
+
+namespace Prime
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            primenum Prime = new primenum();
+            Prime.primenumber();
+        }
+    }
+}
---
+++ Prime/primenum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime
+{
+    public class primenum
+    {
+       public void primenumber()
+        {
+            bool isPrime = true;
+            int i, j;
+            Console.WriteLine("Prime Numbers between 1 to 1000 are ");
+            for (i = 2; i <= 1000; i++)
+            {
+                for (j = 2; j <= 1000; j++)
+                {
+                    if (i != j && i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.Write(" " + i);
+                }
+                isPrime = true;
+            }
+            Console.ReadKey();
+        }
+    }
+}

4ecd0f80209e54b4af8c711c0727900a0454b7e8
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-19 00:34:39 +0530

    [SaiVenkat]-Bubble Sort

---
+++ Problems/BubbleSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem
+{
+    public class BubbleSort
+    {
+        public void Bubble()
+        {
+            int[] arr = { 24, 0, -7, 1, 987, 53 };
+            int temp;
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Bubble Sort : ");
+            foreach (int b in arr)
+            {
+                Console.Write(b + " ");
+            }
+        }
+    }
+}
---
+++ Problems/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Problem;
+
+namespace Problem
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            //primenum Prime = new primenum();
+            //Prime.primenumber();
+
+            BubbleSort Bsort= new BubbleSort();
+            Bsort.Bubble();
+
+        }
+    }
+}
